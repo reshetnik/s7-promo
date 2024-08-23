8f6eb213f0d57b9b7fe9cff1a2cf9d0eb975cfc6
Alexander Reshetnik <reshetnikvsb@gmail.com>
2024-08-23 18:05:28 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `s7-podcast`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
